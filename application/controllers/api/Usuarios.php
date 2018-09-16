bc22521b6422320fea9bed400dee3227bb948cf7
Lucas Jara <lucasjaraespinoza@gmail.com>
2018-09-16 12:22:37 -0300

    se agrega validacion  y envio de datos locales favoritos

--- application/controllers/api/Usuarios.php
+++ application/controllers/api/Usuarios.php
@@ -7,75 +7,83 @@
  */
 
 use Restserver\Libraries\REST_Controller;
-defined('BASEPATH') OR exit('No direct script access allowed');
 
+defined('BASEPATH') OR exit('No direct script access allowed');
 // This can be removed if you use __autoload() in config.php OR use Modular Extensions
 /** @noinspection PhpIncludeInspection */
 //To Solve File REST_Controller not found
 require APPPATH . 'libraries/REST_Controller.php';
 require APPPATH . 'libraries/Format.php';
 
-/**
- * This is an example of a few basic user interaction methods you could use
- * all done with a hardcoded array
- *
- * @package         CodeIgniter
- * @subpackage      Rest Server
- * @category        Controller
- * @author          Phil Sturgeon, Chris Kacerguis
- * @license         MIT
- * @link            https://github.com/chriskacerguis/codeigniter-restserver
- */
-class Usuarios extends REST_Controller {
+class Usuarios extends REST_Controller
+{
     function __construct()
     {
         // Construct the parent class
         parent::__construct();
-
-        // Configure limits on our controller methods
-        // Ensure you have created the 'limits' table and enabled 'limits' within application/config/rest.php
         $this->methods['obtener_usuario']['limit'] = 500; // 500 requests per hour per user/key
-        //$this->methods['users_post']['limit'] = 100; // 100 requests per hour per user/key
-        //$this->methods['users_delete']['limit'] = 50; // 50 requests per hour per user/key
     }
 
-    public function obtener_usuario($id){
-        $this->load->model("/administracion/usuarios_model");
-        $data = $this->usuarios_model->obtener_usuario($id);
-        if (is_null($data)){
-            $this->response($data, REST_Controller::HTTP_OK); // OK (200) being the HTTP response code
-        }else{
-            $this->response(array('error'=>'Error al conectar a la base de datos'),404);
-        }
-    }
     public function obtener_usuario_get()
     {
         $id = $this->get('id');
-
-        // Find and return a single record for a particular user.
-
-        $id = (int) $id;
-
-        // Validate the id.
-        if ($id <= 0)
-        {
-            // Invalid id, set the response and exit.
-            $this->response(NULL, REST_Controller::HTTP_BAD_REQUEST); // BAD_REQUEST (400) being the HTTP response code
+        $id = (int)$id;
+        if ($id <= 0) {
+            $this->response(null, REST_Controller::HTTP_BAD_REQUEST); // BAD_REQUEST (400) being the HTTP response code
         }
-
-        // Get the user from the array, using the id as key for retrieval.
-        // Usually a model is to be used for this.
-        $this->load->model("/administracion/usuarios_model");
+        $this->load->model("/administracion/Usuarios_model", "usuarios_model");
         $data = $this->usuarios_model->obtener_usuario($id);
-
-        if (!empty($data))
-        {
+        //$data = $this->usuarios_model->obtener_usuario($id);
+        if (!empty($data)) {
             $this->set_response($data, REST_Controller::HTTP_OK); // OK (200) being the HTTP response code
-        }
-        else
-        {
-            $arreglo = array('N','Usuario No encontrado');
-            $this->set_response($arreglo, REST_Controller::HTTP_NOT_FOUND); // NOT_FOUND (404) being the HTTP response code
+        } else {
+            $arreglo = array('N', 'Usuario No encontrado');
+            $this->set_response($arreglo,
+                REST_Controller::HTTP_NOT_FOUND); // NOT_FOUND (404) being the HTTP response code
         }
     }
+
+    public function validar_usuario_post()
+    {
+        $usuario = $this->post('usuario');
+        $password = $this->post('password');
+        if ($usuario === null && $password === null) {
+            $this->response([
+                'status' => false,
+                'message' => 'Envie todos los Datos Porfavor'
+            ], REST_Controller::HTTP_NOT_FOUND); // NOT_FOUND (404) being the HTTP response code
+        }
+        $this->load->model("/administracion/Usuarios_model", "usuarios_model");
+        $data = $this->usuarios_model->validar_usuario($usuario, $password);
+        if ($data != null) {
+            $this->set_response(["S", "Credenciales Correctas"],
+                REST_Controller::HTTP_OK); // OK (200) being the HTTP response code
+        } else {
+            $this->set_response(['N', 'Credenciales Incorrectas'],
+                REST_Controller::HTTP_OK); // (200) being the HTTP response code
+        }
+    }
+
+    public function obtener_listado_locales_favoritos_post()
+    {
+        $id = $this->post('id');
+        if ($id <= 0) {
+            $this->response("Error no controlado por sistema",
+                REST_Controller::HTTP_OK); // BAD_REQUEST (400) being the HTTP response code
+        }
+        $this->load->model("/administracion/usuarios_model");
+        $data = $this->usuarios_model->obtener_locales_favoritos($id);
+        $arreglo = array();
+        if ($data != null) {
+            $arreglo[0] = "S";
+            $arreglo[1] = $data;
+            $this->set_response($arreglo, REST_Controller::HTTP_OK); // OK (200) being the HTTP response code
+        } else {
+            $arreglo[0] = "N";
+            $arreglo[1] = 'No cuenta con Locales Favoritos';
+            $this->set_response($arreglo,
+                REST_Controller::HTTP_OK); // (200) being the HTTP response code
+        }
+    }
+
 }
--- application/models/administracion/usuarios_model.php
+++ application/models/administracion/usuarios_model.php
@@ -1,18 +1,42 @@
-<?php
+<?php if (!defined('BASEPATH')) {
+    exit('No direct script access allowed');
+}
+
 /**
  * Created by PhpStorm.
  * User: Lucas
  * Date: 19-08-2018
  * Time: 22:08
  */
-
 class Usuarios_model extends CI_Model
 {
     public function obtener_usuario($id)
     {
+        $this->db->select('*')
+            ->from('tb_usuario')
+            ->where('ID', $id);
+        $query = $this->db->get();
+        return $query->result();
+    }
+
+    public function validar_usuario($usuario, $password)
+    {
         $this->db->select("*")
             ->from('tb_usuario')
-        ->where('ID',$id);
+            ->where('USUARIO', $usuario)
+            ->where('PASSWORD', $password)
+            ->where('ACTIVO', 'S');
+        $query = $this->db->get();
+        return ($query->num_rows() > 0) ? $query->result() : null;
+    }
+
+    public function obtener_locales_favoritos($id)
+    {
+        $this->db->select("locales.NOMBRE LOCAL,locales.DESCRIPCION")
+            ->from('tb_preferencias preferencias')
+            ->join("tb_local locales", "locales.ID=preferencias.TB_LOCAL_ID", 'INNER')
+            ->where('preferencias.ACTIVO', "S")
+            ->where('preferencias.TB_USUARIO_ID', $id);
         $query = $this->db->get();
         return $query->result();
     }
